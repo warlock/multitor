17ef233471de5000ca2f5f7f10efae7a5ac71003
Josep Subils <octozon@gmail.com>
2015-08-14 08:39:08 +0200

    fist version

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # multitor
 Node make Multiple Tor instances
+
+----
+## How to use:
+
+```javascript
+var multitor = require('multitor');
+
+multitor.start(5, function (error, instance, socks_port, control_port) {
+        if (error)  {
+                console.log(error);
+        } else {
+                console.log("Instance " + instance + " > Socks port: " + socks_port + "; Control port: " + control_port);
+        }
+});
+
+```
+
+----
+## How to install:
+```
+npm install multitor
+```
+
+----
--- index.js
+++ index.js
@@ -7,44 +7,29 @@ var control_port = 15000;
 var instances;
 
 exports.start = function(instances, callback) {
-	fs.exists(directory, function (exists) {
-		if (!exists) {
-			fs.mkdir(directory, function () {
-				prepare(instances,callback);
-			});
-		} else {
-			prepare(instances,callback);
+	instances = instances + 1;
+	fs.mkdir(directory, function () {
+		for (var i = 1; i < instances; i++) {
+			prepare(i, instances,callback);
 		}
 	});
 };
 
-var prepare = function(instances, callback) {
-	for (var i = 0;i<instances;i++) {
-		var newdir = directory + "/tor" + i;
-		fs.exists(newdir, function (exists) {
-			if (!exists) {
-				fs.mkdir(newdir, function () {
-					tor(i, callback);
-				});
-			} else {
-				tor(i, callback);
-			}
-		});
-	};
+var prepare = function(i, instances, callback) {
+	var newdir = directory + "/tor" + i;
+	fs.mkdir(newdir, function () {
+		tor(i, callback);
+	});
 };
+
 
 
 var tor = function(i, callback) {
 	var torquery = "tor --RunAsDaemon 1 --CookieAuthentication 0 --HashedControlPassword \"\" --ControlPort " + (control_port + i) + " --PidFile tor" + i + ".pid --SocksPort " + (socks_port + i) + " --DataDirectory " + directory + "/tor" + i;
 	var query = exec(torquery, function (error, stdout, stderr) {
-		console.log('stdout: ' + stdout);
-		console.log('stderr: ' + stderr);
-		if (error !== null) {
-			console.log('exec error: ' + error);
+		if (error) {
+			error = stdout;
 		}
-
-		if ( i === instances ) {
-			callback();
-		}
+		callback(error, i, socks_port+i, control_port+i, instances);
 	});
-}
+};
